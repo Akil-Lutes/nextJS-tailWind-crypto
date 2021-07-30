ce5284a157daeaa86e9fcc5089a53fecf126fc40
Akil Lutes <lutes.akil@gmail.com>
2021-07-30 01:15:02 -0500

    setup await api call

--- pages/index.js
+++ pages/index.js
@@ -11,3 +11,15 @@ export default function Home() {
     </Layout>
   )
 }
+
+export const getServerSideProps = async () => {
+  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+  
+  const organizedCoins = await res.json()
+
+  return {
+    props: {
+      organizedCoins
+    }
+  }
+}
