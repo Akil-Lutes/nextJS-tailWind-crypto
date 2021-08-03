f5ca6784661f076859b5e24e13f88bc2284da903
Akil Lutes <lutes.akil@gmail.com>
2021-08-03 02:16:09 -0500

    Adding in props and styled search row

--- components/Coins/index.js
+++ components/Coins/index.js
@@ -3,6 +3,7 @@
 const Coins = ({
     name,
     price,
+    priceChange,
     symbol,
     marketcap,
     marketcaprank,
@@ -14,8 +15,27 @@ const Coins = ({
 
 }) => {
     return(
-        <div>
-            <h1>{name}</h1>
+        <div className="coin-container flex justify-center border border-yellow-700">
+            <div className="coin-row flex flex-row justify-start items-center h-2/4 w-4/5">
+                <div className="coin flex items-center">
+                    <img src={image} alt={name} className="coin_image" />
+                    <h1 className="coin_name">{name}</h1>
+                    <p className="coin_symbol">{symbol}</p>
+                    <div className="coin_data">
+                        <p className="coin_price">Price:{price}</p>
+                        <p className="coin_ath">All Time High: {alltimehigh}</p>
+                        {priceChange < 0 ? (
+                <p className="coin_percent">
+                  {priceChange.toFixed(2)}%
+                </p>
+              ) : (
+                <p className="coin_percent">
+                  {priceChange.toFixed(2)}%
+                </p>
+              )}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- components/crytpoList.js
+++ components/crytpoList.js
@@ -12,6 +12,7 @@ export default function CryptoList({organizedCoins}) {
                 name={coin.name}
                 id={coin.id}
                 price={coin.current_price}
+                priceChange={coin.price_change_percentage_24h}
                 symbol={coin.symbol}
                 marketcap={coin.market_cap}
                 marketcaprank={coin.market_cap_rank}
