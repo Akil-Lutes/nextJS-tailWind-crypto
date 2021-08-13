0dcf5ba5221f154938d0e24b7c4f64730d1455a4
Akil Lutes <lutes.akil@gmail.com>
2021-08-12 23:54:33 -0500

    created link to coin individual page

--- components/Coins/index.js
+++ components/Coins/index.js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 // My Coins component of what I want to render to the screen
 
 const Coins = ({
@@ -15,32 +17,36 @@ const Coins = ({
 
 }) => {
     return(
-        <div className="coin-container flex justify-center content-evenly w-full md:w-auto">
-            <div className="coin-row flex flex-row justify-start items-center h-20 w-1/3 md:w-5/6 px-8 border-b-2 border-gray-600 hover:bg-green-300">
-                <div className="coin flex items-center pr-8 w-full">
-                    <img src={image} alt={name} className="coin_image h-8 w-8 mr-2.5"/>
-                    <h1 className="coin_name text-base w-40">{name}</h1>
-                    <p className="coin_symbol uppercase">{symbol}</p>
-                    <div className="coin_data flex text-right justify-between w-full">
-                        <p className="coin_price w-32">Price <br></br> ${price}</p>
-                        <p className="coin_ath w-32">All Time High ${alltimehigh}</p>
-                        {priceChange < 0 ? (
-                <p className="coin_percent text-red-600 w-24">
-                  {priceChange.toFixed(2)}%
-                </p>
-              ) : (
-                <p className="coin_percent text-green-400 w-24">
-                  {priceChange.toFixed(2)}%
-                </p>
-              )}
-                        <p className="marketcapRank w-32">Mktcap Rank: {marketcaprank}</p>
-                        <p className="marketCap">Mktcap: ${marketcap.toLocaleString()}</p>
-                        {/* <p className="coin-circSupply">Circulating Supply: {circulatingsupply}</p> */}
-                        {/* <p className="coin-totalSupply">Total Supply: {totalsupply}</p> */}
+      <Link href='/coin/[id]' as={`/coin/{id}`}>
+        <a>
+          <div className="coin-container flex justify-center content-evenly w-full md:w-auto">
+              <div className="coin-row flex flex-row justify-start items-center h-20 w-1/3 md:w-5/6 px-8 border-b-2 border-gray-600 hover:bg-green-300">
+                  <div className="coin flex items-center pr-8 w-full">
+                      <img src={image} alt={name} className="coin_image h-8 w-8 mr-2.5"/>
+                      <h1 className="coin_name text-base w-40">{name}</h1>
+                      <p className="coin_symbol uppercase">{symbol}</p>
+                      <div className="coin_data flex text-right justify-between w-full">
+                          <p className="coin_price w-32">Price <br></br> ${price}</p>
+                          <p className="coin_ath w-32">All Time High ${alltimehigh}</p>
+                          {priceChange < 0 ? (
+                  <p className="coin_percent text-red-600 w-24">
+                    {priceChange.toFixed(2)}%
+                  </p>
+                ) : (
+                  <p className="coin_percent text-green-400 w-24">
+                    {priceChange.toFixed(2)}%
+                  </p>
+                )}
+                            <p className="marketcapRank w-32">Mktcap Rank: {marketcaprank}</p>
+                            <p className="marketCap">Mktcap: ${marketcap.toLocaleString()}</p>
+                            {/* <p className="coin-circSupply">Circulating Supply: {circulatingsupply}</p> */}
+                            {/* <p className="coin-totalSupply">Total Supply: {totalsupply}</p> */}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+          </a>
+      </Link>
     )
 }
 
---
+++ pages/coin/[id].js
@@ -0,0 +1,11 @@
+import Layout from "../../components/layout"
+
+const coin = () => {
+    return(
+        <Layout>
+            <h1>BKC</h1>
+        </Layout>
+    )
+}
+
+export default coin
